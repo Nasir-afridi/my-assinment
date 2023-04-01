79bcd188304c0d40cf361745aba97c2802ac4614
Nasir-afridi <nasirwc05@gmail.com>
2023-04-01 12:35:59 +0500

    collage project

---
+++ tecnologies_in_fuctions.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+void computerinformationtecnology() {
+cout <<"this is \"CIT tecnology\" \n";
+}
+void mechanicaltecnology(){
+    cout <<"this is \"MT tecnology\"  \n";
+}
+void electricaltecnology(){
+    cout <<"this is \"ET tecnology\"  \n";
+}
+void civiltecnology(){
+    cout<<"this is \"CT tecnology\" \n";  
+}
+void autodieseltecnology(){
+    cout <<"this is \"AD tecnology\" \n"; 
+}
+
+int main(){
+computerinformationtecnology();
+mechanicaltecnology();
+electricaltecnology();
+civiltecnology();
+autodieseltecnology();
+    return 0;
+}
