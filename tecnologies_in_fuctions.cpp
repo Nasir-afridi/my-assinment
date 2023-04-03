204f6d659d2bc75b71d3327a110a4a1309781b3a
Nasir-afridi <nasirwc05@gmail.com>
2023-04-03 23:01:07 +0500

    collage project

--- tecnologies_in_fuctions.cpp
+++ tecnologies_in_fuctions.cpp
@@ -1,26 +1,70 @@
-#include<iostream>
-using namespace std;
-void computerinformationtecnology() {
-cout <<"this is \"CIT tecnology\" \n";
-}
-void mechanicaltecnology(){
-    cout <<"this is \"MT tecnology\"  \n";
-}
-void electricaltecnology(){
-    cout <<"this is \"ET tecnology\"  \n";
-}
-void civiltecnology(){
-    cout<<"this is \"CT tecnology\" \n";  
-}
-void autodieseltecnology(){
-    cout <<"this is \"AD tecnology\" \n"; 
-}
+#include<stdio.h>
+    void CIT();
+    void MT();
+    void ATD();
+    void ET();
+    void CT();
 
-int main(){
-computerinformationtecnology();
-mechanicaltecnology();
-electricaltecnology();
-civiltecnology();
-autodieseltecnology();
-    return 0;
-}
+    void CIT(){
+        printf("this is computer information tecnology:\n");
+    }
+    void MT(){
+        printf("this is mechanical tecnology:\n");
+    }
+    void ATD(){
+       printf("this is auto diesel tecnology:\n");
+    }
+    void ET(){
+        printf("this is electrical tecnology:\n");
+    }
+    void CT(){
+        printf("this is civil tecnology\n");
+    }
+    int main(){
+        int number;
+        printf("\n\nthis is computer informaton tecnology:\n");
+        printf("this is mechanical tecnology:\n") ;
+         printf("this is auto diesel tecnology:\n");
+         printf("this is electrical tecnology:\n");
+         printf("this is civil tecnology:\n\n");
+         printf("enter your number:");
+       scanf("%s",&number);
+
+        switch(number){
+        case '1':{
+            CIT();   
+         break;
+         }
+         case '2':
+         {
+            MT();
+         break;
+         }
+         
+         case '3':
+         {
+             ATD();
+         break;
+         }
+        
+         case '4':
+         {
+            ET();
+         break;
+         }
+         
+         case '5':
+         {
+             CT();
+         break;
+         }
+        
+         default:
+         {
+            printf("recheck your entered number:\n");
+            }
+        
+        }
+        printf("written by M.Nasir:\n\n");
+        return 0;
+        }
